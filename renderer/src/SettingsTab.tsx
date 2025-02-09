88f31c96ffa20ef3332aa8448aada482c59886e4
svatal <swata@centrum.cz>
2025-02-09 20:25:32 +0100

    display settings

--- renderer/src/SettingsTab.tsx
+++ renderer/src/SettingsTab.tsx
@@ -1,5 +1,41 @@
 import * as b from "bobril";
+import type { ISettings } from "../../main/sharedTypes";
 
 export function SettingsTab() {
-  return <div>TODO</div>;
+  const [settings, setSettings] = b.useState<ISettings>({
+    templates: {},
+    vouchers: {},
+  });
+
+  b.useEffect(() => {
+    window.settings.get().then((settings) => {
+      setSettings(settings);
+    });
+  }, []);
+
+  return (
+    <div>
+      {Object.entries(settings.templates).map(([templateId, template]) => (
+        <div key={templateId}>
+          <h3>{template.filename}</h3>
+          <ul>
+            {Array.from({ length: template.pageCount }, (_, pageIndex) => (
+              <li key={pageIndex}>
+                {pageIndex + 1}:
+                {Object.entries(settings.vouchers)
+                  .filter(
+                    ([_, voucher]) =>
+                      voucher.templateId === templateId &&
+                      voucher.page === pageIndex
+                  )
+                  .map(([voucherId, voucher]) => (
+                    <span key={voucherId}> Name: {voucher.name}</span>
+                  ))}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
