c9b8d6695bd1e15f2d294494f36ab33d8acae9b9
svatal <swata@centrum.cz>
2025-02-27 19:01:26 +0100

    voucher code filter

--- renderer/src/VouchersTab.tsx
+++ renderer/src/VouchersTab.tsx
@@ -1,5 +1,5 @@
 import * as b from "bobril";
-import { ISettings, IVoucher } from "../../main/sharedTypes";
+import { ISettings } from "../../main/sharedTypes";
 
 export function VouchersTab() {
   const [settings, setSettings] = b.useState<ISettings>({
@@ -10,21 +10,32 @@ export function VouchersTab() {
   b.useEffect(() => {
     window.settings.get().then(setSettings);
   }, []);
+  const codeFilter = b.useState("");
+  const entries = b.useMemo(() => {
+    const lcCodeFilter = codeFilter().toLocaleLowerCase();
+    return Object.entries(settings.vouchers)
+      .filter(([_, voucher]) =>
+        voucher.code.toLocaleLowerCase().includes(lcCodeFilter)
+      )
+      .sort(([_keyA, a], [_keyB, b]) => b.createdAtTicks - a.createdAtTicks);
+  }, [settings, codeFilter()]);
   return (
-    <table>
-      <thead>
-        <tr style={{ textAlign: "left" }}>
-          <th>Code</th>
-          <th>Valid until</th>
-          <th>Template</th>
-          <th>Note</th>
-          <th>Redeemed</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Object.entries(settings.vouchers)
-          .sort((a, b) => b[1].createdAtTicks - a[1].createdAtTicks)
-          .map(([id, voucher]) => (
+    <>
+      <div>
+        Code: <input type="text" value={codeFilter} />
+      </div>
+      <table>
+        <thead>
+          <tr style={{ textAlign: "left" }}>
+            <th>Code</th>
+            <th>Valid until</th>
+            <th>Template</th>
+            <th>Note</th>
+            <th>Redeemed</th>
+          </tr>
+        </thead>
+        <tbody>
+          {entries.map(([id, voucher]) => (
             <tr key={id}>
               <td>{voucher.code}</td>
               <td>{voucher.validUntil}</td>
@@ -33,7 +44,8 @@ export function VouchersTab() {
               <td>{voucher.isRedeemed ? "Yes" : "No"}</td>
             </tr>
           ))}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </>
   );
 }
